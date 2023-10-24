bf498ab16d87dea9141b9d1ce040650dcb564995
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-24 18:04:47 -0500

    update to client/webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const srcDir = './src/js/';
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -10,15 +11,42 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: `${srcDir}index.js`,
+      install: `${srcDir}install.js`,
+      database: `${srcDir}database.js`,
+      editor: `${srcDir}editor.js`,
+      header: `${srcDir}header.js`
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE",
+        desscription: 'Text Editor with Javascript',
+        background_color: '#663300',
+        theme_color:'#663300',
+        start_url: './',
+        publicPath: './',
+        icons:[
+          {
+            src: path.resolve('src/images.logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assest', 'icons')
+          }
+        ]
+      })
     ],
 
     module: {
