33135c9e75123c1021f84fcbf9bff5527273231b
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-30 05:32:22 -0500

    Update database.js add error handling  putDb&getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,25 +13,47 @@ const initdb = async () =>
   });
 
 export const putDb = async (content) => {
-  console.log('content', content)
-  console.log('Putting to database..');
-  const jateDb = await openDB('jate', 1,);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({Value: content});
-  const result = await request;
-  console.log('Data has been saved to database', result);
+  try {
+    if(content === null) {
+      console.log("No Data to save");
+    } else {
+    console.log('content', content)
+    console.log('Putting to database..');
+    const jateDb = await openDB('jate', 1,);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({Value: content});
+    const result = await request;
+    console.log('Data has been saved to database', result);
+    }
+    
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 export const getDb = async () => {
-  console.log('Getting data from database...');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.getAll();
-  const result = await request;
-  console.log('Retrieved Data', result);
-  return result;
+  try {
+    console.log('Getting data from database...');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+
+    if(result.length === 0){
+      console.log("No Data");
+    } else{
+    console.log('Retrieved Data', result);
+    return result
+    }
+
+    return result;
+  } catch (error) {
+    console.log(error)
+  }
 };
+
+
 
 initdb();
