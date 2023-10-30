7b32d8f18a05bc466a0d8d498ef2ea9c764fb453
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2023-10-30 04:57:56 -0500

    fix typo prevntng logo load, fix database.js/putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,23 +12,19 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  // debugger
   console.log('content', content)
-  console.log('Putting to database...');
+  console.log('Putting to database..');
   const jateDb = await openDB('jate', 1,);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({value: content});
+  const request = store.put({Value: content});
   const result = await request;
-  console.log('Data has been saved to database')
+  console.log('Data has been saved to database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('Getting data from database...');
-  // debugger
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
